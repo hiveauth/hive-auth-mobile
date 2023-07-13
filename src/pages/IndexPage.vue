c205e875e1be010d6a59e4fc8929739373277610
Sagar <sagar.devices@gmail.com>
2023-07-13 20:41:30 +0530

    layout done for importing keys

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,49 +1,61 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+  <q-page>
+    <div class="q-pa-lg">
+      <q-input
+        outlined
+        rounded
+        v-model="data.hiveusername"
+        label="Username"
+        placeholder="Enter Hive Username"
+      >
+        <template v-slot:prepend>
+          <q-icon name="alternate_email" />
+        </template>
+      </q-input>
+      <q-input
+        outlined
+        rounded
+        v-model="data.hiveuserkey"
+        type="password"
+        label="Private Key"
+        placeholder="Enter Private Key"
+        class="q-pt-lg"
+      >
+        <template v-slot:prepend>
+          <q-icon name="key" />
+        </template>
+      </q-input>
+
+      <div class="row q-mt-lg">
+        <q-btn
+          class="col q-pt-sm q-pb-sm"
+          rounded
+          color="primary"
+          label="Import"
+        />
+      </div>
+
+      <div class="row q-mt-lg">
+        <div class="col text-subtitle1 text-center">
+          Add your existing HIVE account to Keychain
+        </div>
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script lang="ts">
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
 import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
   name: 'IndexPage',
-  components: { ExampleComponent },
-  setup () {
-    const todos = ref<Todo[]>([
-      {
-        id: 1,
-        content: 'ct1'
-      },
-      {
-        id: 2,
-        content: 'ct2'
-      },
-      {
-        id: 3,
-        content: 'ct3'
-      },
-      {
-        id: 4,
-        content: 'ct4'
-      },
-      {
-        id: 5,
-        content: 'ct5'
-      }
-    ]);
-    const meta = ref<Meta>({
-      totalCount: 1200
+  components: {},
+  setup() {
+    const data = ref({
+      hiveusername: '',
+      hiveuserkey: '',
     });
-    return { todos, meta };
-  }
+    return { data };
+  },
 });
 </script>
