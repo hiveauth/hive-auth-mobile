59cd5a11c1f3b918df967d7b41d19db3ff378c51
Sagar <sagar.devices@gmail.com>
2023-07-29 22:03:39 +0530

    implemented lock unlock.

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -21,65 +21,81 @@
         class="bg-primary text-white"
         v-if="hasAuthStore.isUnlocked"
       >
-        <q-list padding style="margin-top: 155px; height: calc(100% - 155px)">
-          <q-item clickable v-ripple>
-            <q-item-section avatar>
-              <q-icon name="inbox" />
-            </q-item-section>
-            <q-item-section> Inbox </q-item-section>
-          </q-item>
+        <q-scroll-area style="height: calc(100% - 100px); margin-top: 100px">
+          <q-list padding>
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="wallet" />
+              </q-item-section>
 
-          <q-item clickable v-ripple>
-            <q-item-section avatar>
-              <q-icon name="star" />
-            </q-item-section>
+              <q-item-section> Wallet </q-item-section>
+            </q-item>
 
-            <q-item-section> Star </q-item-section>
-          </q-item>
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="people" />
+              </q-item-section>
 
-          <q-item clickable v-ripple>
-            <q-item-section avatar>
-              <q-icon name="send" />
-            </q-item-section>
+              <q-item-section> Manage Accounts </q-item-section>
+            </q-item>
 
-            <q-item-section> Send </q-item-section>
-          </q-item>
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="qr_code_scanner" />
+              </q-item-section>
 
-          <q-item clickable v-ripple>
-            <q-item-section avatar>
-              <q-icon name="send" />
-            </q-item-section>
+              <q-item-section> Scan QR </q-item-section>
+            </q-item>
 
-            <q-item-section> Send </q-item-section>
-          </q-item>
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="info" />
+              </q-item-section>
 
-          <q-item clickable v-ripple>
-            <q-item-section avatar>
-              <q-icon name="drafts" />
-            </q-item-section>
+              <q-item-section> About </q-item-section>
+            </q-item>
 
-            <q-item-section> Drafts </q-item-section>
-          </q-item>
-        </q-list>
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="settings" />
+              </q-item-section>
+
+              <q-item-section> Settings </q-item-section>
+            </q-item>
+
+            <q-item clickable v-ripple @click="lockApp">
+              <q-item-section avatar>
+                <q-icon name="lock" />
+              </q-item-section>
+
+              <q-item-section> Lock </q-item-section>
+            </q-item>
+          </q-list>
+        </q-scroll-area>
 
         <q-img
           class="absolute-top"
           src="https://cdn.quasar.dev/img/material.png"
-          style="height: 155px"
+          style="height: 100px; width: 100%"
         >
           <div class="bg-transparent">
             <div class="q-mt-lg"></div>
-            <q-avatar size="40px" class="q-mb-sm">
-              <q-img
-                class="iv-logo-color"
-                src="src/icon/AppIcon.png"
-                width="40px"
-                height="40px"
-              />
-            </q-avatar>
-            <div class="text-weight-bold">Hive Auth Signer</div>
-            <div>@arcange</div>
-            <div>@sagarkothari88</div>
+            <div class="row">
+              <div class="q-pr-sm q-pt-sm">
+                <q-avatar size="40px" class="q-mb-sm">
+                  <q-img
+                    class="iv-logo-color"
+                    src="src/icon/AppIcon.png"
+                    width="40px"
+                    height="40px"
+                  />
+                </q-avatar>
+              </div>
+              <div class="col q-pt-sm">
+                <div class="text-weight-bold">Hive Auth Signer</div>
+                <div>@arcange, @sagarkothari88</div>
+              </div>
+            </div>
           </div>
         </q-img>
       </q-drawer>
@@ -95,6 +111,7 @@
 import { defineComponent, ref } from 'vue';
 import { useHasPathStore } from 'src/stores/has-path';
 import { useHasAuthStore } from 'src/stores/has-auth';
+import { useRouter } from 'vue-router';
 
 export default defineComponent({
   name: 'MainLayout',
@@ -104,10 +121,17 @@ export default defineComponent({
   setup() {
     const hasPathStore = useHasPathStore();
     const hasAuthStore = useHasAuthStore();
+    const router = useRouter();
     const data = ref({
       isDrawerOpen: false,
     });
-    return { data, hasPathStore, hasAuthStore };
+
+    function lockApp() {
+      hasAuthStore.lockApp();
+      router.push({ name: 'passcode-lock' });
+    }
+
+    return { data, hasPathStore, hasAuthStore, lockApp };
   },
   mounted() {
     this.hasAuthStore.readKeys();
