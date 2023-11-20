f8a167cc42591c9e46311d3294e178183b3f47a8
Sagar <sagar.devices@gmail.com>
2023-11-20 23:27:38 +0530

    Plugins development.

--- src-capacitor/ios/App/App/PluginViewController.swift
+++ src-capacitor/ios/App/App/PluginViewController.swift
@@ -6,9 +6,86 @@
 //
 
 import UIKit
+import WebKit
 
 class PluginViewController: UIViewController {
+    let app = "app"
+    let config = WKWebViewConfiguration()
+    let rect = CGRect(x: 0, y: 0, width: 10, height: 10)
+    var webView: WKWebView?
+    var didFinish = false
+    var handlers: [String: ((String) -> Void)] = [: ]
+
+//    var getProofOfKeyHandler: ((String) -> Void)? = nil
+//    var decryptHandler: ((String) -> Void)? = nil
+//    var encryptHandler: ((String) -> Void)? = nil
+//    var getPublicKeyHandler: ((String) -> Void)? = nil
+//    var signChallengeHandler: ((String) -> Void)? = nil
+//    var validateHiveKeyHandler: ((String) -> Void)? = nil
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    func setupWebView() {
+        config.userContentController.add(self, name: app)
+        webView = WKWebView(frame: rect, configuration: config)
+        webView?.navigationDelegate = self
+//#if DEBUG
+        if #available(iOS 16.4, *) {
+            self.webView?.isInspectable = true
+        }
+//#endif
+        guard
+            let url = Bundle.main.url(forResource: "index", withExtension: "html", subdirectory: "HiveAuth")
+        else { return }
+        let dir = url.deletingLastPathComponent()
+        webView?.loadFileURL(url, allowingReadAccessTo: dir)
+    }
+}
+
+extension PluginViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        didFinish = true
+    }
+}
+
+extension PluginViewController: WKScriptMessageHandler {
+    func userContentController(
+        _ userContentController: WKUserContentController,
+        didReceive message: WKScriptMessage
+    ) {
+        guard message.name == app else { return }
+        guard let dict = message.body as? [String: AnyObject] else { return }
+        guard let type = dict["type"] as? String else { return }
+        guard let error = dict["error"] as? String else { return }
+        guard let data = dict["data"] as? String else { return }
+        guard let id = dict["id"] as? String else { return }
+        guard let jsonData = try? JSONEncoder().encode([
+            "error": error,
+            "data": data,
+            "type": type,
+            "id": id,
+        ]) else { return }
+        guard let jsonString = String(data: jsonData, encoding: .utf8) else { return }
+//        switch type {
+//        case "getProofOfKey":
+//            getProofOfKeyHandler?(jsonString)
+//        case "decrypt":
+//            decryptHandler?(jsonString)
+//        case "encrypt":
+//            encryptHandler?(jsonString)
+//        case "getPublicKey":
+//            getPublicKeyHandler?(jsonString)
+//        case "signChallenge":
+//            signChallengeHandler?(jsonString)
+//        case "validateHiveKey":
+//            validateHiveKeyHandler?(jsonString)
+//        default:
+//            debugPrint("Do Nothing")
+//        }
+        guard let handler = handlers[id] else { return }
+        handler(jsonString)
+        handlers[id] = nil
     }
 }
--- src-capacitor/ios/App/App/Plugins/EchoPlugin.swift
+++ src-capacitor/ios/App/App/Plugins/EchoPlugin.swift
@@ -9,17 +9,58 @@ import Foundation
 
 import Capacitor
 
-@objc(HiveAuthPlugin)
-public class HiveAuthPlugin: CAPPlugin {
+@objc(HASCustomPlugin)
+public class HASCustomPlugin: CAPPlugin {
+
+    var pluginViewController = UIStoryboard(
+        name: "Main",
+        bundle: nil
+    ).instantiateViewController(
+        withIdentifier: "PluginViewController"
+    ) as? PluginViewController
 
     public override func load() {
         debugPrint("Plugin is loaded")
+        super.load()
+        pluginViewController?.viewDidLoad()
     }
 
-    @objc func echo(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
-        call.resolve([
-            "value": value
-        ])
+    @objc func callPlugin(_ call: CAPPluginCall) {
+        guard
+            let callId = call.getString("callId"),
+            let method = call.getString("method"),
+            let privateKey = call.getString("privateKey"),
+            let publicKey = call.getString("publicKey"),
+            let memo = call.getString("memo"),
+//            let accountName = call.getString("accountName"),
+//            let userKey = call.getString("userKey"),
+            let challenge = call.getString("challenge"),
+            let key = call.getString("key")
+        else { return }
+
+        pluginViewController?.handlers[callId] = { result in
+            call.resolve([
+                "callId": callId,
+                "info": [
+                    "dataString": result
+                ]
+            ])
+        }
+
+        if method == "getProofOfKey" {
+            pluginViewController?.webView?
+                .evaluateJavaScript("getProofOfKey('\(privateKey)','\(publicKey)','\(memo)');")
+        } else if method == "signChallenge" {
+            pluginViewController?.webView?
+                .evaluateJavaScript("signChallenge('\(challenge)','\(key)');")
+        } else if method == "decrypt" {
+            pluginViewController?.webView?
+                .evaluateJavaScript("decrypt('\(challenge)','\(privateKey)');")
+        } else if method == "getPublicKey" {
+            pluginViewController?.webView?
+                .evaluateJavaScript("getPublicKey('\(privateKey)');")
+        } else if method == "getDeepLinkData" {
+            // TO-DO: - How do we do this? 🤷‍♂️
+        }
     }
 }
